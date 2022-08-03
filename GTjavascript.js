c25049dbc1064bc7fdaf11cd90e71c3f3bfbdbb0
Ivan Voxgaard <ivan.voxgaard@gmail.com>
2022-08-02 23:28:49 -0400

    day_1_commit

---
+++ GTjavascript.js
@@ -0,0 +1,20 @@
+// exercise 1
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+// exercise 2
+let ourPlanet;
+let currentUser;
+
+//exercise 3
+let answer = prompt(Who is the Drizzle?);
+
+if(answer === 'Carl') { 
+    alert("Damn straight.");
+}
+   else {
+    alert("NAH");
+     }
