4636ef12a1fd2a699fdba62c645ddda786aa609a
Ivan Voxgaard <ivan.voxgaard@gmail.com>
2022-10-15 17:17:45 -0400

    Campaigns.php + Accordion(Carousel), changed color schema + background image, +assets

---
+++ directory/campaigns.php
@@ -0,0 +1,130 @@
+<?php include 'included/header.php' ?>
+<h2>Grand Strategy Campaign Templates</h2>
+<!-- Accordian Segment 1 Begin -->
+<div class="accordion" id="accordionCabinet">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Europa Universalis 4 Campaign Templates
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionCabinet">
+      <div class="accordion-body">
+        <!-- Carousel -->
+        <div id="carouselCampaignTemplatesEur" class="carousel slide" data-bs-ride="carousel" style="height:500px; width:700px;">
+          <ol class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide-to="2"></button>
+            <button type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide-to="3"></button>
+            <button type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide-to="4"></button>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="../assets/images/austria_70.jpg" style="height:500px; width:500px;"alt="First slide">
+              <div class="carousel-caption">
+                <h3>Emperor</h3>
+                <p>A pivotal era ignites the Holy Roman Empire, taxed by threats from within and without.  Is this empire a thorn in your side or a weapon to wield?</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../assets/images/qing_70.jpg" style="height:500px; width:500px;"alt="Second slide">
+              <div class="carousel-caption">
+                <h3>Mandate of Heaven</h3>
+                <p>Pretenders to the celestial throne must strike a balance between their personal ambitions and external forces which threaten their common ruin.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../assets/images/aztec_70.jpeg" style="height:500px; width:500px;"alt="Second slide">
+              <div class="carousel-caption">
+                <h3>Conquest of Paradise</h3>
+                <p>Not even the vast expanses of the so-called "New World" can sate the jealous claws of empire.  Can you balance European affairs with your colonial expansion?</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../assets/images/england_70.jpeg" style="height:500px; width:500px;"alt="Second slide">
+              <div class="carousel-caption">
+                <h3>Rule Britannia</h3>
+                <p>Celtic upstarts, entrenched Anglo-Saxons, and Scandinavian reavers all contest rule over the British isles.  The land is in short supply but there is room enough on the seas for them all to forge an empire.</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../assets/images/persia_70.png" style="height:500px; width:500px;"alt="Third slide">
+              <div class="carousel-caption">
+                <h3>Cradle of Civilization</h3>
+                <p>All imperial yokes upon Arabia have fallen away in the hour of Rome's extinction.  Can those who invented empire learn to escape it?</p>
+              </div>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselCampaignTemplatesEur" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+          </button>
+        </div>
+        <!-- Carousel End -->
+      </div>
+    </div>
+  </div>
+  <!-- Accordian Segment 2 Begin 
+  <div class="accordion" id="accordionPanelTwo"> -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Crusader Kings 3 Campaign Templates
+        </button>
+      </h2>
+      <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionCabinet">
+        <div class="accordion-body">
+        <!-- Carousel -->
+          <div id="carouselCampaignTemplatesCru" class="carousel slide" data-bs-ride="carousel" style="height:500px; width:700px;">
+            <ol class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselCampaignTemplatesCru" data-bs-slide-to="0" class="active"></button>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="../assets/images/norse_70.jpg" style="height:500px; width:500px;"alt="First slide">
+                <div class="carousel-caption">
+                  <h3>TBA: In Development</h3>
+                  <p>Developing consensus on what rules should be modded into this game.  Also, still testing campaign scenarios.</p>
+                </div>
+              </div>
+            </div>
+        <!-- Carousel End -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Accordian Segment 3 Begin 
+  <div class="accordion" id="accordionPanelThree"> -->
+    <div class="accordion-item">
+      <h2 class="accordion-header" id="headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Victoria 3 Campaign Templates
+        </button>
+      </h2>
+      <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionCabinet">
+        <div class="accordion-body">
+        <!-- Carousel -->
+          <div id="carouselCampaignTemplatesVic" class="carousel slide" data-bs-ride="carousel" style="height:500px; width:700px;">
+            <ol class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselCampaignTemplatesVic" data-bs-slide-to="0" class="active"></button>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="../assets/images/victoria_70.jpg" style="height:500px; width:500px;"alt="First slide">
+                <div class="carousel-caption">
+                  <h3>TBA: Awaiting Game Release</h3>
+                  <p>Developing consensus on what rules should be modded into this game.  Also, still developing campaign scenarios.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        <!-- Carousel End -->
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Accordian End -->
+<?php include 'included/footer.php' ?>
--- directory/included/header.php
+++ directory/included/header.php
@@ -4,14 +4,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>-->
   <title>GameTactica</title>
   <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
+  <link rel="stylesheet" href="../GTstyle.css">
   <script src="/static/search.js"></script>
 </head>
 <body>
   <?php include '../config/database.php' ?>
-  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+  <nav class="navbar navbar-expand-sm navbar-light mb-4 bg-warning">
     <div class="container">
       <a class="navbar-brand" href="./GTindex.html">GameTactica Campaigns</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -38,5 +41,5 @@
 </nav>
 
 <main>
-  <div class="container d-flex flex-column align-items-center">
-    <img src="../assets/images/GT_LOGO.jpg" class="w-10 mb-3" alt="Decorative GT Logo">
+  <div class="container d-flex flex-column align-items-center bg-secondary py-3">
+    <img src="../assets/images/gt_logo.jpg" class="w-10 mb-3" alt="Decorative GT Logo">
